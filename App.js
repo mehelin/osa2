e23745983fb4b4b8dae4eec851502075ed47f461
MH <86976920+mehelin@users.noreply.github.com>
2021-10-31 18:58:45 +0200

    App.js maat
    Alustus maat tehtävään

--- App.js
+++ App.js
@@ -1,89 +1,142 @@
+
+
+/*
 import React, { useState, useEffect } from 'react'
+import Note from './components/Note'
 import axios from 'axios'
-import Filter from './components/Filter';
-import Persons from './components/Persons';
-import PersonForm from './components/PersonForm';
-
-
 
 const App = () => {
-  const [persons, setPersons] = useState([])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [newFilter, setNewFilter] = useState('')
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(false)
 
   useEffect(() => {
     console.log('effect')
     axios
-      .get('http://localhost:3001/persons')
+      .get('https://restcountries.com/v3.1/all')
       .then(response => {
         console.log('promise fulfilled')
-        setPersons(response.data)
+        setNotes(response.data)
       })
   }, [])
-
-console.log('render', persons.length,'persons')
-  
-
-// window.confirm() kehottaa selainta näyttämään valintaikkunan, 
-// jossa on valinnainen viesti, ja odottamaan, kunnes käyttäjä 
-// joko vahvistaa tai peruuttaa valintaikkunan.
-
-  const addPerson = (event) => {
+  console.log('render', countries.length, 'maat')
+/*
+  const addNote = (event) => {
     event.preventDefault()
-    if (persons.some(person => person.name === newName)) {
-        window.alert(`${newName} on jo puhelinluettelossa`)
-        return
-    }
-    const personObject = {
-        name: newName,
-        number: newNumber,
-        date: new Date().toISOString(),
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() > 0.5,
+      id: notes.length + 1,
     }
 
-    setPersons(persons.concat(personObject))
-    setNewName('')
-    setNewNumber('')
-}
-
-
-  const handlePersonChange = (event) => {
-    setNewName(event.target.value)
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
   }
 
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
   }
 
-  const handleFilterChange = (event) => {
-    setNewFilter(event.target.value)
-
-  }
-
+  const notesToShow = showAll
+  ? notes
+  : notes.filter(note => note.important)
 
   return (
     <div>
-      <h1>Puhelinluettelo</h1>
-
-      <Filter persons={newFilter}
-      handleFilterChange={handleFilterChange}
-      />
-
-      <h1>Lisää uusi</h1>
-
-      <PersonForm persons={persons} 
-      setPersons={setPersons} 
-      setNewName={setNewName}  
-      setNewNumber={setNewNumber}
-      handlePersonChange={handlePersonChange} 
-      newNumber={newNumber} 
-      handleNumberChange={handleNumberChange} 
-      newName={newName} addPerson={addPerson}
-      />
-      <h1>Numerot</h1>
-      <Persons persons={persons} newFilter={newFilter}/>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>   
+      <ul>
+        {notesToShow.map(note => 
+            <Note key={note.id} note={note} />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form>  
     </div>
   )
+}
+
+export default App
+*/
+
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Country from './components/Country'
+import CountryList from './components/CountryList'
+
+// Jos ehdon täyttäviä maita on liikaa (yli 10), kehoitetaan tarkentamaan hakuehtoa:
+const SearchedCountries = ({selectedCountry, countries}) => {
+  const filteredCountries = countries.filter(country => country.name.toLowerCase().includes(selectedCountry.toLowerCase()))
+  
+  if (filteredCountries.length > 10) {
+    return (
+      'Liikaa osumia, määritä toinen suodatin'
+    )
+  }
+  if (filteredCountries.length > 1 ) {
+    return (
+      // näyttää luettelon maista, jotka vastaavat ehtoja
+    filteredCountries.map(selectedCountry => <CountryList key={selectedCountry.name} country={selectedCountry}></CountryList>) 
+    )
+  }
+  if (filteredCountries.length === 1) {
+    const oneCountry = filteredCountries[0]
+    return (
+      <Country country={oneCountry}></Country>
+    )
+  } else {
+    return null
+  }
+
+}
+
+const App = () => {
+
+  const [countries, setCountries ] = useState ([])
+  const [ selectedCountry, setSelected ] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('https://restcountries.eu/v3.1/all')
+      .then(response => {
+        console.log('promise fulfilled')
+        setCountries(response.data)
+      })
+  }, [])
+  console.log('render', countries.length, 'countries')
+
+  
+const handleSelectedChange = (event) => {
+  console.log(event.target.value)
+    setSelected(event.target.value)
+}
+
+// Kun ehdon täyttäviä maita on enää yksi, näytetään maan perustiedot, lippu sekä siellä puhutut kielet:
+return (
+  <div>
+    
+      Etsi maita <input value={selectedCountry} onChange={handleSelectedChange} ></input>
+    <div>
+      <SearchedCountries selectedCountry={selectedCountry} countries={countries}></SearchedCountries>
+
+    </div>
+    
+  </div>
+)
+
 }
 
 export default App
