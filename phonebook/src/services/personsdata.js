74dc11e47f5b9a6d350db1d267ede8b93f75f734
MH <86976920+mehelin@users.noreply.github.com>
2021-11-03 23:50:37 +0200

    Update personsdata.js

--- phonebook/src/services/personsdata.js
+++ phonebook/src/services/personsdata.js
@@ -1,23 +1,25 @@
 import axios from 'axios'
-const baseUrl = '/api/notes'
+const baseUrl = 'http://localhost:3001/persons'
 
 const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
+    const request = axios.get(baseUrl)
+    return request.then(response => response.data)
+  }
+  
+  const create = newObject => {
+    const request = axios.post(baseUrl, newObject)
+    return request.then(response => response.data)
+  }
+  
+  const remove = (id, newObject) => {
+    const request = axios.delete(`${baseUrl}/${id}`, newObject)
+    return request.then(response => response.data)
+  }
 
-const create = newObject => {
-  const request = axios.post(baseUrl, newObject)
-  return request.then(response => response.data)
-}
-
-const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject)
-  return request.then(response => response.data)
-}
-
-// eslint-disable-next-line import/no-anonymous-default-export
-export default { 
-  getAll, create, update
-}
-   
+  const update = (id, newObject) => {
+    const request = axios.put(`${baseUrl}/${id}`, newObject)
+    return request.then(response => response.data)
+  }
+  
+  // eslint-disable-next-line import/no-anonymous-default-export
+  export default { getAll, create, remove, update }
