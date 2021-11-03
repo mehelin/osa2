5756ff70937838fc1d8d1a3adf04df1a973febda
MH <86976920+mehelin@users.noreply.github.com>
2021-11-03 21:07:11 +0200

    Update notes.js

--- phonebook/src/services/notes.js
+++ phonebook/src/services/notes.js
@@ -16,6 +16,7 @@ const update = (id, newObject) => {
   return request.then(response => response.data)
 }
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default { 
   getAll, create, update
 }
